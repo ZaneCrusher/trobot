67219b8405952540f58ebefe745b3823508be41c
jhsea3do <jhsea3do@3345c356-724b-0410-8fd4-79f76bf19138>
2008-04-25 10:03:10 +0000

    info village

---
+++ trunk/src/java/main/com/javaws/trobot/InfoVillage.java
@@ -0,0 +1,199 @@
+/*
+ * ~ TROBOT ~
+ * 
+ * $Id$
+ */
+
+package com.javaws.trobot;
+
+/**
+ * 
+ * @author jhsea3do
+ * 
+ */
+public class InfoVillage extends InfoKarte {
+
+	/**
+	 * resource types
+	 */
+	final public static String[] RS_TYPES = { "WOOD", "CLAY", "IRON", "CROP" };
+
+	/**
+	 * village name
+	 */
+	private String name;
+
+	/**
+	 * resource upper limits
+	 */
+	private int[] rsLimits = new int[RS_TYPES.length];
+
+	/**
+	 * resource actual counts
+	 */
+	private int[] rsActuals = new int[RS_TYPES.length];
+
+	/**
+	 * resource increase counts
+	 */
+	private int[] rsIncreases = new int[RS_TYPES.length];
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+
+		this.name = name;
+	}
+
+	/**
+	 * @return the rsActuals
+	 */
+	public int[] getRsActuals() {
+
+		return rsActuals;
+	}
+
+	/**
+	 * @return the rsIncreases
+	 */
+	public int[] getRsIncreases() {
+
+		return rsIncreases;
+	}
+
+	/**
+	 * @return the rsLimits
+	 */
+	public int[] getRsLimits() {
+
+		return rsLimits;
+	}
+
+	/**
+	 * @return the rsActual
+	 */
+	public int getRsActual(int type) {
+
+		return rsActuals[type];
+	}
+
+	/**
+	 * @return the rsIncrease
+	 */
+	public int getRsIncrease(int type) {
+
+		return rsIncreases[type];
+	}
+
+	/**
+	 * @return the rsLimit
+	 */
+	public int getRsLimit(int type) {
+
+		return rsLimits[type];
+	}
+
+	/**
+	 * @return the rsActual
+	 */
+	public void setRsActual(int type, int value) {
+
+		rsActuals[type] = value;
+	}
+
+	/**
+	 * @return the rsIncrease
+	 */
+	public void setRsIncrease(int type, int value) {
+
+		rsIncreases[type] = value;
+	}
+
+	/**
+	 * @return the rsLimit
+	 */
+	public void setRsLimit(int type, int value) {
+
+		rsLimits[type] = value;
+	}
+
+	/**
+	 * @return the rsActual
+	 */
+	public void setRsActual(int type, String value) {
+
+		setRsActual(type, Integer.parseInt(value));
+	}
+
+	/**
+	 * @return the rsIncrease
+	 */
+	public void setRsIncrease(int type, String value) {
+
+		setRsIncrease(type, Integer.parseInt(value));
+	}
+
+	/**
+	 * @return the rsLimit
+	 */
+	public void setRsLimit(int type, String value) {
+
+		setRsLimit(type, Integer.parseInt(value));
+	}
+
+	/**
+	 * copy properties from anohter village
+	 * 
+	 * @param village
+	 */
+	public void copy(InfoVillage village) {
+
+		for (int i = 0; i < this.rsActuals.length; i++) {
+			if (null != village.rsActuals) {
+				this.rsActuals[i] = village.rsActuals[i];
+			}
+			if (null != village.rsIncreases) {
+				this.rsIncreases[i] = village.rsIncreases[i];
+			}
+			if (null != village.rsLimits) {
+				this.rsLimits[i] = village.rsLimits[i];
+			}
+		}
+
+		if (null != village.name) {
+			this.name = village.name;
+		}
+
+		if (null != village.getD()) {
+			this.setD(village.getD());
+		}
+
+		if (null != village.getC()) {
+			this.setC(village.getC());
+		}
+
+		if (null != village.getPosX()) {
+			this.setPosX(village.getPosX());
+		}
+
+		if (null != village.getPosY()) {
+			this.setPosY(village.getPosY());
+		}
+
+		if (null != village.getType()) {
+			this.setType(village.getType());
+		}
+
+	}
+
+}
--- trunk/src/java/main/com/javaws/trobot/htmlcleaner/TrobotHtmlCleanerImpl.java
+++ trunk/src/java/main/com/javaws/trobot/htmlcleaner/TrobotHtmlCleanerImpl.java
@@ -6,10 +6,13 @@
 
 package com.javaws.trobot.htmlcleaner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,6 +25,7 @@ import com.javaws.trobot.HtmlUtils;
 import com.javaws.trobot.InfoAllianz;
 import com.javaws.trobot.InfoKarte;
 import com.javaws.trobot.InfoSpieler;
+import com.javaws.trobot.InfoVillage;
 import com.javaws.trobot.Trobot;
 
 public class TrobotHtmlCleanerImpl implements Trobot {
@@ -32,7 +36,12 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 
 	private static HtmlUtils htmlUtils = HtmlUtils.getInstance();
 
+	static {
+		htmlUtils.setUserAgent(config.getString("trobot.agent"));
+	}
+
 	public TrobotHtmlCleanerImpl() {
+
 		super();
 		this.username = config.getSiteUser();
 		this.password = config.getSitePass();
@@ -40,10 +49,10 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 	}
 
 	public TrobotHtmlCleanerImpl(String uri) {
+
 		this();
 	}
 
-	@SuppressWarnings("unused")
 	private String cookie = null;
 
 	private String username = "no_user";
@@ -52,7 +61,16 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 
 	private boolean debug = false;
 
+	private String active_village_did = null;
+
+	private String server_time = null;
+
+	private String client_time = null;
+
+	private Map<String, InfoVillage> villages = null;
+
 	public void debug(String message, String fromCharset, String toCharset) {
+
 		if (debug) {
 			try {
 				String line = new String(message.getBytes(fromCharset),
@@ -66,9 +84,181 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 	}
 
 	public void debug(String message) {
+
 		if (debug) {
 			System.out.println(message);
 		}
+	}
+
+	protected void debugHeader(HttpMethod method) {
+
+		if (debug) {
+			Header[] requestheaders = method.getRequestHeaders();
+			for (Header header : requestheaders) {
+				debug("<<< request header [name=" + header.getName()
+						+ ", value=" + header.getValue() + "]");
+			}
+			Header[] responseheaders = method.getResponseHeaders();
+			for (Header header : responseheaders) {
+				debug(">>> response header [name=" + header.getName()
+						+ ", value=" + header.getValue() + "]");
+			}
+		}
+	}
+
+	protected void refreshCookie(HttpMethod method) {
+
+		String lastCookie = this.cookie;
+		this.cookie = htmlUtils.getCookieText(method);
+		if (debug && null != this.cookie && !this.cookie.equals(lastCookie)) {
+			debug(">>> refresh cookie = " + cookie);
+		}
+	}
+
+	protected void addVillage(InfoVillage village) {
+
+		if (null == this.villages) {
+			this.villages = new LinkedHashMap<String, InfoVillage>();
+		}
+		String did = village.getD();
+		if (null == did) {
+			log.error("village has no did [village=" + village + "]");
+			return;
+		}
+		InfoVillage v = this.villages.get(did);
+		if (null != v) {
+			v.copy(village);
+		} else {
+			v = village;
+		}
+		debug(">>> user village [did=" + v.getD() + ", name=" + v.getName()
+				+ "]");
+		this.villages.put(did, v);
+	}
+
+	protected InfoVillage getActiveVillage() {
+
+		if (null != this.active_village_did && null != this.villages) {
+			return this.villages.get(this.active_village_did);
+		} else {
+			return null;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void loadVillages(Document page) throws Exception {
+
+		Element elem_lright1 = (Element) page
+				.selectSingleNode("//div[@id=\"lright1\"]");
+		if (null != elem_lright1) {
+			// multi villages
+			// debug(">>> elem_lright1 = " + elem_lright1.asXML());
+			List<Element> elem_village_trs = elem_lright1
+					.selectNodes("//table[@class=\"f10\"]/tbody/tr");
+			for (Element elem_village_tr : elem_village_trs) {
+				// debug(">>> elem_village_tr = " +
+				// elem_village_tr.asXML());
+				Element elem_nbr_td = null;
+				Element elem_right_td = null;
+				List<Element> elem_village_tr_tds = elem_village_tr
+						.elements("td");
+				// elem_nbr_td = (Element) elem_village_tr
+				// .selectSingleNode("/td[@class=\"nbr\"]");
+				for (Element elem_village_tr_td : elem_village_tr_tds) {
+					String attr_td_class = elem_village_tr_td
+							.attributeValue("class");
+					if ("nbr".equals(attr_td_class)) {
+						elem_nbr_td = elem_village_tr_td;
+					} else if ("right".equals(attr_td_class)) {
+						elem_right_td = elem_village_tr_td;
+					}
+				}
+				if (null != elem_nbr_td && null != elem_right_td) {
+					// debug(">>> elem_nbr_td = " + elem_nbr_td.asXML());
+					Element elem_a = elem_nbr_td.element("a");
+					if (null != elem_a) {
+						// <a href="?newdid=154477">3.¿À¬˛¥Û÷Ì</a>
+						String v_did = elem_a.attributeValue("href")
+								.replaceAll("\\?newdid=", "");
+						String v_name = elem_a.getTextTrim();
+						InfoVillage village = new InfoVillage();
+						village.setD(v_did);
+						village.setName(v_name);
+						if ("active_vl".equals(elem_a.attributeValue("class"))) {
+							this.active_village_did = v_did;
+							InfoVillage v = this.loadActiveVillage(page);
+							village.copy(v);
+						}
+						this.addVillage(village);
+					}
+					// debug(">>> elem_right_td = " +
+					// elem_right_td.asXML());
+				}
+			}
+		} else {
+			// single village
+			this.active_village_did = "single";
+			InfoVillage village = this.loadActiveVillage(page);
+			village.setD(this.active_village_did);
+			this.addVillage(village);
+		}
+
+		InfoVillage activeVillage = this.getActiveVillage();
+		debug(">>> active village " + activeVillage.getName());
+	}
+
+	protected void loadServerTiming(Document page) throws Exception {
+
+		Element elem_ltime_tp1 = (Element) page
+				.selectSingleNode("//div[@id=\"ltime\"]/span[@id=\"tp1\"]");
+		this.server_time = elem_ltime_tp1.getTextTrim();
+		this.client_time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+		debug(">>> server time " + this.server_time);
+		debug("<<< client time " + this.client_time);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected InfoVillage loadActiveVillage(Document page) throws Exception {
+
+		InfoVillage village = new InfoVillage();
+		// Element elem_lmid1 = (Element)
+		// page.selectSingleNode("//div[@id=\"lmid1\"]");
+		Element elem_dname = (Element) page
+				.selectSingleNode("//div[@id=\"lmid2\"]/div[@class=\"dname\"]/h1");
+		String v_name = elem_dname.getTextTrim();
+		village.setName(v_name);
+		debug(">>> user village [name=" + village.getName() + "]");
+		List<Element> elem_rs_ic_trs = page
+				.selectNodes("//div[@id=\"lrpr\"]/table/tbody/tr");
+		if (null != elem_rs_ic_trs
+				&& elem_rs_ic_trs.size() == InfoVillage.RS_TYPES.length) {
+			for (int i = 0; i < elem_rs_ic_trs.size(); i++) {
+				Element elem_rs_ic_td = (Element) elem_rs_ic_trs.get(i)
+						.elements("td").get(2);
+				String rs_ic = elem_rs_ic_td.element("b").getTextTrim()
+						.replaceAll(".$", "");
+				village.setRsIncrease(i, rs_ic);
+			}
+		}
+		List<Element> elem_rs_tds = page
+				.selectNodes("//div[@id=\"lres0\"]/table/tbody/tr/td[@id]");
+		if (null != elem_rs_tds
+				&& elem_rs_tds.size() == InfoVillage.RS_TYPES.length) {
+			for (int i = 0; i < elem_rs_ic_trs.size(); i++) {
+				Element elem_rs_td = elem_rs_tds.get(i);
+				String[] rs_ac_and_lm = elem_rs_td.getTextTrim().split("/");
+				village.setRsActual(i, rs_ac_and_lm[0]);
+				village.setRsLimit(i, rs_ac_and_lm[1]);
+			}
+		}
+		for (int i = 0; i < InfoVillage.RS_TYPES.length; i++) {
+
+			debug(">>> " + InfoVillage.RS_TYPES[i].toLowerCase()
+					+ " status [increase=" + village.getRsIncrease(i)
+					+ ", actual=" + village.getRsActual(i) + ", limit="
+					+ village.getRsLimit(i) + "]");
+		}
+		return village;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -80,7 +270,7 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 			debug("<<< login from " + url_login);
 			debug("<<< login username " + this.username,
 					Configuration.CHARSET_DEFAULT, Configuration.CHARSET_REMOTE);
-			debug("<<< login password " + this.password,
+			debug("<<< login password " + this.password.replaceAll(".", "*"),
 					Configuration.CHARSET_DEFAULT, Configuration.CHARSET_REMOTE);
 			String html_login = htmlUtils.getResponseText(htmlUtils
 					.load(url_login));
@@ -91,6 +281,8 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 			Document page_login = htmlUtils.getResponseDocument(xml_login);
 			Element form_login = (Element) page_login
 					.selectSingleNode("//form[@name=\"snd\"]");
+			String url_action = "/" + form_login.attributeValue("action");
+			debug("<<< post url " + url_action);
 			// debug(">>> login form " + form_login.asXML());
 			// debug(">>> login response " + page_login.asXML());
 			List<Element> elem_inputs = form_login.selectNodes("//input");
@@ -100,62 +292,79 @@ public class TrobotHtmlCleanerImpl implements Trobot {
 				// debug(">>> input field " + elem_input.asXML());
 				String key = elem_input.attributeValue("name");
 				String value = elem_input.attributeValue("value");
+				String debug_value = value;
 				String input_type = elem_input.attributeValue("type");
 				if ("text".equals(input_type) || "password".equals(input_type)) {
 					String input_class = elem_input.attributeValue("class");
 					if ("fm fm110".equals(input_class)) {
 						if (input_type.equals("text")) {
 							value = this.username;
+							debug_value = value;
 						} else if (input_type.equals("password")) {
 							value = this.password;
+							debug_value = value.replaceAll(".", "*");
 						}
 					}
 				}
-				debug("<<< post param key=" + key + " ,value=" + value);
+				debug("<<< post param [key=" + key + ", value=" + debug_value
+						+ "]", Configuration.CHARSET_DEFAULT,
+						Configuration.CHARSET_REMOTE);
 				input_params.put(key, value);
 			}
-			HttpMethod method_submit = htmlUtils.load(config
-					.getUrl("/dorf1.php"), input_params, header_params, "post");
-			// String html_submit = htmlUtils.getResponseText(method_submit);
-			// Header[] method_submit_headers =
-			// method_submit.getResponseHeaders();
-			this.cookie = htmlUtils.getCookieText(method_submit);
-			debug(">>> set-cookie = " + cookie);
+			HttpMethod method_submit = htmlUtils.load(
+					config.getUrl(url_action), input_params, header_params,
+					"post");
+			String html_submit = htmlUtils.getResponseText(method_submit);
+			HtmlCleaner hc_submit = new HtmlCleaner(html_submit);
+			hc_submit.clean();
+			String xml_submit = hc_submit.getXmlAsString();
+			Document page_submit = htmlUtils.getResponseDocument(xml_submit);
+			this.loadVillages(page_submit);
+			this.loadServerTiming(page_submit);
+			// debug(">>> submit response = " + xml_submit);
+			// this.debugHeader(method_submit);
+			this.refreshCookie(method_submit);
 			login_successful = true;
 		} catch (Exception ex) {
 			this.cookie = null;
 			login_successful = false;
-			log.error("login failed by " + ex.getMessage());
+			log.error("login failed: " + ex.getMessage(), ex);
 		}
 		return login_successful;
 	}
 
 	public InfoAllianz allianz(String uri) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public InfoAllianz allianz(int aid) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public InfoKarte karte(String uri) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public InfoKarte karte(String d, String c) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public InfoSpieler spieler(String uri) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public InfoSpieler spieler(int uid) throws Exception {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
